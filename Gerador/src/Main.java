170ee8e3f1621abcbbf23bc4c884fa8bdb97eae9
unknown <iagevlinsingen@gmail.com>
2026-05-21 21:11:20 -0300

    Testing JFugue and IntelliJ IDE.

---
+++ Gerador/src/Main.java
@@ -0,0 +1,9 @@
+import org.jfugue.player.Player;
+
+class Main {
+    static void main() {
+        Player player = new Player();
+        player.play("C5");
+        System.out.println("feito!");
+    }
+}
